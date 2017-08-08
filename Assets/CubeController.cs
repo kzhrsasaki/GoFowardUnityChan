d2808b17a8b3bd343d0afb79a97b07284983ced2
kzhr.sasaki <kzhr.sasaki@gmail.com>
2017-08-08 16:37:54 +0900

    課題の提出

---
+++ Assets/CubeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour {
+
+	// キューブの移動速度
+	private float speed = -0.2f;
+
+	// 消滅位置
+	private float deadLine = -10;
+
+
+	//課題
+	public bool characterInQuicksand;
+	void OnCollisionEnter2D(Collision2D other) {
+		characterInQuicksand = true;
+
+		Debug.Log ("OnCollisionEnter2D");
+
+		if (other.gameObject.tag == "GroundTag" || other.gameObject.tag == "Cubetag") {
+			gameObject.GetComponent<AudioSource> ().Play();
+		
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		    // キューブを移動させる
+		    transform.Translate (this.speed, 0, 0);
+
+		    // 画面外に出たら破棄する
+		    if (transform.position.x < this.deadLine){
+			              Destroy (gameObject);
+		}
+	}
+}
